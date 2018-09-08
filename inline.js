944eea00edcee19fda4bc4508e748951342fbb9d
unknown <laupatrik@gmail.com>
2018-09-08 13:25:01 +0300

    inline
    and alib.js

---
+++ inline.js
@@ -0,0 +1,16 @@
+<script>
+(function(w,d,u,c){w.ttdl=[];var s,r,t;r=false;s=d.createElement('script');s.type='text/javascript';s.src=u;s.onload=s.onreadystatechange=function(){if(!r&&(!this.readyState||this.readyState=='complete'))
+{r=true;c();}};t=d.getElementsByTagName('script')[0];t.parentNode.insertBefore(s,t);}
+(window,document,'https://localhost/js/alib.js',function(){
+// Tracking code reference	
+var ttid='1482259877-ef07557fu6';
+// Custom page name
+var pn=false;
+var allowTracking=true;
+var autoTrack=true;
+var useCookies=true;
+if(typeof window.ttlib!='undefined'){
+ttlib.init([ttid,false,true,pn,false,true,autoTrack],[allowTracking,useCookies]);
+ttlib.pageview();
+}}));
+</script>
